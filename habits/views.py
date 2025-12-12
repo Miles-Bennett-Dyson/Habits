f3ae82638a92849063d9bae205b30b100f423e1f
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-12 17:45:50 +0500

    добавлено разрешение в эндпоинты

--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from habits.models import Habits
 from habits.paginators import HabitPaginator
+from habits.permissions import IsOwner
 from habits.serialaizers import HabitSerializer
 
 
@@ -12,3 +14,16 @@ class HabitsVewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         habit = serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+
+        if self.action in ['update', 'partial_update']:
+            self.permission_classes = [IsAuthenticated, IsOwner]
+
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, IsOwner]
+
+        elif self.action == 'retrieve':
+            self.permission_classes = [IsAuthenticated, IsOwner]
+
+        return [permission() for permission in self.permission_classes]
