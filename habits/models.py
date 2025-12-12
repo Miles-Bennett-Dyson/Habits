e34820db876f6cd3498177aba2f42fb9839c9aa5
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-12 14:29:40 +0500

    сделано приложение привычек, описана модель

---
+++ habits/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+class Habits(models.Model):
+
+    PERIODICITY_CHOICES = [
+        (1, 'Каждые понедельник, среда, пятница'),
+        (2, 'Каждые вторник, четверг, суббота'),
+        (3, 'Каждый будний день'),
+        (4, 'Каждые выходные'),
+        (5, 'Ежедневно')
+    ]
+
+    owner = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        verbose_name="Автор записи",
+        related_name="habit",
+        blank=True,
+        null=True
+    )
+    place = models.CharField(
+        max_length=255,
+        verbose_name="Место",
+        help_text="Введите место, в котором необходимо выполнять привычку"
+    )
+    time = models.DateTimeField(
+        verbose_name="Время",
+        help_text="Введите время, когда необходимо выполнять привычку"
+    )
+    action = models.CharField(
+        max_length=255,
+        verbose_name="Действие",
+        help_text="Введите действие, которое представляет собой привычка."
+    )
+    is_pleasure = models.BooleanField(
+        default=False,
+        verbose_name="Признак приятной привычки"
+    )
+    related_habit = models.ForeignKey( # Связанная привычка
+        "self",
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name="main_habit",
+        verbose_name="Связанная привычка",
+        help_text= "Укажите связанную привычку"
+    )
+    periodicity = models.PositiveIntegerField(
+        default=5,
+        choices=PERIODICITY_CHOICES,
+        verbose_name="Периодичность",
+        help_text = "Укажите периодичность выполнения привычки для напоминания в днях"
+    )
+    reward = models.CharField(
+        max_length=255,
+        **NULLABLE,
+        verbose_name="Вознаграждение",
+        help_text="Укажите чем себя вознаградить после выполнения"
+    )
+    duration = models.DurationField(
+        verbose_name="Длительность выполнения",
+        help_text = "Укажите время, которое предположительно потратите на выполнение привычки."
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name="Признак публичности",
+        help_text = "Укажите можно ли опубликовать привычку в общий доступ, чтобы другие пользователи смогли взять её себе в пример."
+    )
+
+    def __str__(self):
+        return self.action
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
+        ordering = ('pk',)
