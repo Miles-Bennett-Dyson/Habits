04069e37637aa51b65852cbc37a6ebf7f6ac59aa
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-12 17:35:34 +0500

    сделано добавление владельца при создании привычки

--- habits/views.py
+++ habits/views.py
@@ -7,3 +7,6 @@ from habits.serialaizers import HabitSerializer
 class HabitsVewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habits.objects.all()
+
+    def perform_create(self, serializer):
+        habit = serializer.save(owner=self.request.user)
