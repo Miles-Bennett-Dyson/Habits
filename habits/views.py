e3d7481de7c98ec7f51b3d5dfaa8eb567c3f812d
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-12 17:37:15 +0500

    добавлен пагинатор

--- habits/views.py
+++ habits/views.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets
 
 from habits.models import Habits
+from habits.paginators import HabitPaginator
 from habits.serialaizers import HabitSerializer
 
 
 class HabitsVewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habits.objects.all()
+    pagination_class = HabitPaginator
 
     def perform_create(self, serializer):
         habit = serializer.save(owner=self.request.user)
