ce01de82d23a617e29db6de99b30570cb4840ebe
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-13 13:22:12 +0500

    написана сервисная функция для отправки уведомлений в телеграм

---
+++ habits/services.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def send_telegram_message(chat_id, message):
+    """ Функция, для отправки уведомлений в чат telegram. """
+    params = {
+        'text': message,
+        'chat_id': chat_id,
+    }
+    url = 'https://api.telegram.org/bot'
+    token = '8057904554:AAGnsyt552hxhsN6tRdB0q2PPhmkrqruPrU'
+
+    response = requests.get(f'{url}{token}/sendMessage', params=params)
+
+if __name__ == '__main__':
+    send_telegram_message(1225099467, 'По лбу не дало?')
