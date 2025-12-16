640ff93a2491e173cb75052e3ed38b0dee7dcc12
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-16 18:29:59 +0500

    добавлено поле tg_chat_id в профиль пользователя

--- users/models.py
+++ users/models.py
@@ -1,12 +1,35 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="Электронная почта")
-    phone_number = models.CharField(max_length=15, verbose_name="Номер телефона", blank=True, null=True)
-    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True)
-    city = models.CharField(max_length=30, verbose_name="Город", blank=True, null=True)
-    username = models.CharField(max_length=30, verbose_name="Никнейм", blank=True, null=True)
+    phone_number = models.CharField(
+        max_length=15,
+        verbose_name="Номер телефона",
+        **NULLABLE,
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватар",
+        **NULLABLE,
+    )
+    city = models.CharField(
+        max_length=30,
+        verbose_name="Город",
+        **NULLABLE,
+    )
+    username = models.CharField(
+        max_length=30,
+        verbose_name="Никнейм",
+        **NULLABLE,
+    )
+    tg_chat_id = models.PositiveIntegerField(
+        verbose_name="ID чата в телеграмм",
+        help_text="Введите ID чата в телеграмм",
+        **NULLABLE,
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
