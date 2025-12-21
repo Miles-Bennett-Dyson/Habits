4d7dae572c424d6d80bc3dfc100739c16fa10bce
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-12-21 11:01:05 +0500

    добавлен тест изменения привычки

--- habits/tests.py
+++ habits/tests.py
@@ -15,10 +15,11 @@ class HabitCRUDTestCase(APITestCase):
             place='Бассейн',
             time=datetime.time(20, 30),
             action='Плавать',
-            is_pleasure=True,
             periodicity=1,
-            duration='600',
-            is_public=True
+            duration='120',
+            is_pleasure=True,
+            is_public=True,
+            owner=self.user
         )
         self.client.force_authenticate(user=self.user)
 
@@ -39,6 +40,16 @@ class HabitCRUDTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Habits.objects.all().count(), 2)
+
+    def test_habit_edit(self):
+        """ Тест изменения привычки """
+        url = reverse(viewname='habits:habit-detail', args=(self.habit.pk,))
+        data = {
+            'duration': '60',
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Habits.objects.get(place='Бассейн').duration, datetime.timedelta(seconds=60))
 
     def test_is_pleasure_and_reward(self):
         """ Тест одновременного указания вознаграждения и приятной привычки. """
@@ -158,7 +169,7 @@ class HabitCRUDTestCase(APITestCase):
         }
         response = self.client.post(url, data, format='json')
         validation_error_text = response.json().get('non_field_errors')[0]
-        expected_text = 'Время на выполнение не должны быть более 120 секунд!'
+        expected_text = 'Время на выполнение не должно быть более 120 секунд!'
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(validation_error_text, expected_text)
