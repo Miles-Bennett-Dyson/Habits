d563b4d815975ce6faac68c0b086def2d75c0a68
Denis Gabdrakhmanov <rubik5000@ya.ru>
2026-01-06 15:40:51 +0500

    исправления кода

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -8,13 +8,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            user = User.objects.create(email='super@user.com', username = 'Admin')
+            user = User.objects.create(email='super@user.com', username='Admin')
             user.is_staff = True
             user.is_active = True
             user.is_superuser = True
             user.set_password('admin')
             user.save()
-            self.stdout.write(self.style.SUCCESS('Супер-пользователь создан. \n email = super@user.com \n password =  admin '))
+            self.stdout.write(
+                self.style.SUCCESS('Супер-пользователь создан. \n email = super@user.com \n password =  admin ')
+            )
         except Exception as e:
             self.stdout.write(
                 self.style.DANGER(f'Ошибка создания супер-пользователя: {e}'))
